984c804134dd60c8c7f08eee13f54763338f7ee9
HideyukiKita <kitatakikamo@gmail.com>
2026-01-15 20:28:03 +0900

    Add:POST:todoの作成API追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 ////////////////////////////////////
 // サーバー作成用モジュール
 ///////////////////////////////////
-
 import express from 'express';
 import { pool } from './db.js';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+//PostgreSQLのTIMESTAMPを日本時間の文字列に変換する共通フォーマット
+const JST_TIME_FORMAT = (column: string) => `TO_CHAR(${column} AT TIME ZONE 'JST', 'YYYY-MM-DD HH24:MI:SS')`;
+
+// Todoテーブルの標準的な選択カラム(日本時間変換込み)
+const TODO_COLUMNS = `
+    id, title, memo, priority, is_completed,
+    ${JST_TIME_FORMAT('deadline')} as deadline,
+    ${JST_TIME_FORMAT('created_at')} as created_at
+`;
 
 // ESMで__dirnameを再現するための設定
 const __filename = fileURLToPath(import.meta.url);
@@ -21,12 +30,15 @@ const PORT = process.env.SERVER_PORT || 3000;
 // Webサーバーの土台作成
 const app = express();
 
+// JSONを受け取れるようミドルウェア追加
+app.use(express.json());
+
 // テスト用API(DBの現在日時取得)
 app.get('/api/healthcheck', async (req, res) => {
     try {
         // DBの現在日時の取得
         const result = await pool.query(
-             `SELECT TO_CHAR(NOW() AT TIME ZONE 'JST', 'YYYY-MM-DD HH24:MI:SS') as jst_time`
+             `SELECT ${JST_TIME_FORMAT('NOW()')} as jst_time`
         );
         res.status(200).json({
             status: 'OK',
@@ -38,20 +50,16 @@ app.get('/api/healthcheck', async (req, res) => {
     }
 });
 
+
 // [GET] /api/todos Todo一覧取得API
 app.get('/api/todos', async (req, res) => {
     try {
         // SQLの設定
         const query = `
             SELECT
-                id,
-                title,
-                memo,
-                priority,
-                is_completed,
-                TO_CHAR(deadline AT TIME ZONE 'JST', 'YYYY-MM-DD HH24:MI:SS') as deadline,
-                TO_CHAR(created_at AT TIME ZONE 'JST', 'YYYY-MM-DD HH24:MI:SS') as created_at
-            FROM todos
+                ${TODO_COLUMNS}
+            FROM 
+                todos
             WHERE
                 deleted_at IS NULL
             ORDER BY
@@ -67,6 +75,28 @@ app.get('/api/todos', async (req, res) => {
     }
 });
 
+
+//[POST]Todo新規登録API
+app.post('/api/todos', async (req, res) => {
+    // 登録データの変数化
+    const { title, memo, priority, deadline } = req.body;
+
+    try {
+        const query = `
+            INSERT INTO todos (title, memo, priority, deadline)
+            VALUES ($1, $2, $3, $4)
+            RETURNING ${TODO_COLUMNS};
+        `;
+        const values = [title, memo, priority, deadline];
+        const result = await pool.query(query, values);
+
+        // 作成されたデータを返す
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(`Todo登録エラー: ${error}`);
+        res.status(500).json({error: 'データの登録に失敗しました。'});
+    }
+});
 
 
 app.listen(PORT, () => {
